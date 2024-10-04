8140936fe8f7f01081f5227ec1c2c969fd875575
gamenumber <kimhunbin0226@gmail.com>
2024-10-04 10:55:30 +0900

    삼항 연산 사용

--- WEB-UI/01-GNB/02/js/script.js
+++ WEB-UI/01-GNB/02/js/script.js
@@ -21,13 +21,17 @@ $(function () {
     // 마우스 휠을 조작했을 때
     $window.on("wheel", function (e) {
         // console.log(e);
-
-        if (e.originalEvent.wheelDelta > 0) {
-            // 휠을 올렸을 때
-            $header.removeClass("hide");
-        } else {
-            // 휠을 내렸을 때
-            $header.addClass("hide");
-        }
+        // if (e.originalEvent.wheelDelta > 0) {
+        //     // 휠을 올렸을 때
+        //     $header.removeClass("hide");
+        // } else {
+        //     // 휠을 내렸을 때
+        //     $header.addClass("hide");
+        // }
+        // 삼항 조건 연산자
+        // 조건 ? 참일 때 동작 : 거짓일 때 동작 (삼항 연산 조건문) -> 한 줄로 쓸 수 있어서 매우 좋음
+        e.originalEvent.wheelDelta > 0
+            ? $header.removeClass("hide")
+            : $header.addClass("hide");
     });
 });

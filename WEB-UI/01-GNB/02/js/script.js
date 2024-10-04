4d8fb9125173db048e3837af9894444caaf547a4
gamenumber <kimhunbin0226@gmail.com>
2024-10-04 10:47:02 +0900

    최종완성

--- WEB-UI/01-GNB/02/js/script.js
+++ WEB-UI/01-GNB/02/js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+    const $window = $(window);
     const $header = $("header");
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
@@ -10,9 +11,23 @@ $(function () {
         $header.addClass("active");
         $submenu.stop().slideDown(duration);
     });
+
     $menu.on("mouseleave", function () {
         $(this).removeClass("on");
         $header.removeClass("active");
         $submenu.stop().slideUp(duration);
+    });
+
+    // 마우스 휠을 조작했을 때
+    $window.on("wheel", function (e) {
+        // console.log(e);
+
+        if (e.originalEvent.wheelDelta > 0) {
+            // 휠을 올렸을 때
+            $header.removeClass("hide");
+        } else {
+            // 휠을 내렸을 때
+            $header.addClass("hide");
+        }
     });
 });

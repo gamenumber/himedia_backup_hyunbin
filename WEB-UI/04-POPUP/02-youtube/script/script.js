c3ad8532e86b39d3f31a58b5947b0bdf43611eeb
gamenumber <kimhunbin0226@gmail.com>
2024-10-08 12:20:46 +0900

    자동재생 및 뮤비타이틀 넣기

--- WEB-UI/04-POPUP/02-youtube/script/script.js
+++ WEB-UI/04-POPUP/02-youtube/script/script.js
@@ -11,10 +11,17 @@ $(function () {
     // 비디오 리스트를 선택했을 때
     $videoItem.on("click", function () {
         // console.log($videoItem, $(this));
-        const videoLink = $(this).attr("data-link");
+        let videoLink = $(this).attr("data-link");
 
+        // 자동재생 추가
+        // A += B --> A = A + B
+        videoLink += "?autoplay=1";
         // iframe의 src 값으로 videoLink를 전달
-        $video.arrt("src", videoLink);
+        $video.attr("src", videoLink);
+
+        // 텍스트 받아서 videoTitle 변수에 담기
+        const videoTitle = $(this).text();
+        $caption.text(videoTitle);
 
         // 팝업창 띄우기
         $dim.fadeIn();
@@ -26,6 +33,7 @@ $(function () {
     $btnClose.on("click", function () {
         $dim.fadeOut();
         $videoWrap.removeClass("active");
+        $video.attr("src", "");
     });
 
     // setTimeout(동작, 시간)

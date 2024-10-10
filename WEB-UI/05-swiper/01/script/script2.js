637a72dc2fc920384b6a85b36b78053cf1ad0011
gamenumber <kimhunbin0226@gmail.com>
2024-10-10 11:57:35 +0900

    swiper.js적용

--- WEB-UI/05-swiper/01/script/script2.js
+++ WEB-UI/05-swiper/01/script/script2.js
@@ -7,24 +7,13 @@ const topSlider = new Swiper(".top-slider", {
     effect: "slide", // slide, fade, cube, flip, coverflow
 
     // 캐러셀 만들기
-    slidesPerView: 1, // 보여질 슬라이드 갯수
+    slidesPerView: 3, // 보여질 슬라이드 갯수
     spaceBetween: 20, // 간격 20px
-    centeredSlides: true, // 첫번째 슬라이드를 가운데로
-
-    // RWD setting
-    breakpoints: {
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1200: {
-            slidesPerView: 5,
-        },
-    },
 
     // If we need pagination
     pagination: {
         el: ".top-slider-wrap .pagination",
+        clickable: true,
     },
 
     // Navigation arrows
